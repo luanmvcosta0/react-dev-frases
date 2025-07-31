156a566b3db113a94560de05437388f7ff25b75e
Luan <luanmvcosta@gmail.com>
2025-07-31 11:46:34 -0300

    Criando e estilizando projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import { useState } from 'react';
 import './App.css';
 
+import logoImg from './assets/logo.png'
+
 function App() {
 
   return (
-    <div></div>
+    <div className='container'>
+      <img 
+        src={logoImg}
+        alt="Logo frases"
+        className='logo'
+        />
+
+        <h2 className='title'>Categorias</h2>
+        <section className='category-area'>
+          <button className='category-button'>Motivação</button>
+          <button className='category-button'>Bem estar</button>
+        </section>
+
+        <button className='button-frase'>Gerar frase</button>
+
+        <p className='texto-frase'>Alguma frase vai vir aqui</p>
+
+    </div>
   );
 };
 
